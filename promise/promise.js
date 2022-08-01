0f4ff4cb97d5eb11168c8c0cf4b7fffb5c639743
liujiana <liujiana@yy.com>
2022-08-01 16:28:06 +0800

    feat: 新增promise文件

--- promise/promise.js
+++ promise/promise.js
@@ -1,3 +1,5 @@
+import promiseAplusTests from "promises-aplus-tests";
+
 const Promise_State = {
   PENDING: "pending",
   FULFILLED: "fulfilled",
@@ -101,12 +103,98 @@ class MyPromise {
         const thenResult = onFn(fnVal);
         if (thenResult instanceof MyPromise) {
           if (prevPromise === thenResult) {
-
+            // 2.3.1
+            reject(new TypeError());
+          } else {
+            // 2.3.2
+            thenResult.then(resolve, reject);
           }
+        } else {
+          // 2.3.3
+          if (typeof(thenResult) === "Object" || typeof(thenResult) === "Function") {
+            // 2.3.3.1
+            const thenFunction = thenResult.then;
+            if (typeof(thenFunction) === "Function") {
+              const [resolvePromise, rejectPromise] = this._runBothOneTimeFunction((result) => {
+                if (!this._thenableResolve(result, resolve, reject)) {
+                  resolve(result);
+                }
+              }, (errorReason) => {
+                reject(errorReason);
+              })
+
+              try {
+                thenFunction.call(thenResult, resolvePromise, rejectPromise);
+              } catch (e) {
+                // 2.3.3.2
+                rejectPromise(e);
+              }
+              return;
+            }
+          }
+
+          // 2.3.3.4
+          // 2.3.4
+          resolve(thenResult);
         }
       } catch (e) {
-        
+        reject(e);
       }
     })
   }
+
+  _runThenRejected(thenFn) {
+    const onFulfilledFn = thenFn[0];
+    const [resolve, reject] = this._runBothOneTimeFunction(thenFn[2][1], thenFn[2][2]);
+    if (!onFulfilledFn || typeof(onFulfilledFn) !== "Function") {
+      // 2.2.73
+      resolve(this.result);
+    } else {
+      this._runThenWrap(onFulfilledFn, this.result, thenFn[2][0], resolve, reject);
+    }
+  }
+
+  _runMicroTask(fn) {
+    // 2.2.4
+    queueMicrotask(fn);
+  }
+
+  _checkStateCanChange() {
+    // 2.1.1
+    return this.state === Promise_State.PENDING;
+  }
+
+  then(onFulfilled, onRejected) {
+    const nextThen = [];
+    const nextPromise = new MyPromise((resolve, reject) => {
+      nextThen[1] = resolve;
+      nextThen[2] = reject;
+    });
+    nextThen[0] = nextPromise;
+
+    // 2.2.6
+    this.thenSet.push([onFulfilled, onRejected, nextThen]);
+    this._runMicroTask(() => this._tryRunThen());
+    return nextThen[0];
+  }
 }
+
+function typeOf(check) {
+  const type = Object.prototype.toString.call(check);
+  return type.match(/\[object\ (.*)\]/)[1];
+}
+
+MyPromise.defer = MyPromise.deferred = function () {
+  let dtd = {};
+  dtd.promise = new MyPromise((resolve, reject) => {
+    dfd.resolve = resolve;
+    dfd.reject = reject;
+  });
+  return dfd;
+}
+
+export default MyPromise;
+
+describe("Promises/A+ Tests", function () {
+  promiseAplusTests.mocha(MyPromise);
+})
